2961cff91d210a3e7b8fab9e66d83fc2918502dc
seyithansaltturk <seyithansaltturk@gmail.com>
2022-11-22 19:31:55 +0300

    güncel

--- app_api/controllers/yorumlar.js
+++ app_api/controllers/yorumlar.js
@@ -1,59 +1,61 @@
-var mongoose = require('mongoose');
-var Mekan=mongoose.model("mekan");
-const cevapOlustur=function(res,status,content){
-    res
-    .status(status)
-    .json(content);
-}
+var mongoose = require("mongoose");
+var Mekan = mongoose.model("mekan");
 
-const yorumEkle=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const cevapOlustur = function(res,status,content){
+    res.status(status).json(content);
 }
-
-const yorumSil=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const yorumEkle = function(req,res){
+    cevapOlustur(res,200,{"durum":"basarili"});
 }
-
-const yorumGuncelle=function(req,res){
-    cevapOlustur(res,200,{"durum":"başarılı"});
+const yorumSil = function(req,res){
+    cevapOlustur(res,200,{"durum":"basarili"});
 }
-
-const yorumGetir=function(req,res){
-    if(req.params && req.params.mekanid && req.params.yorumid){
-        Mekan.findById(req.params.mekanid)
-        .select("ad yorumlar")
-        .exec(function(hata,mekan){
-            var cevap,yorum;
-            if(!mekan){
-                cevapOlustur(res,404,{"hata": "Böyle bir mekan yok"});
-                return;
-            } else if(hata){
-                cevapOlustur(res,400,hata);
-                return;
+const yorumGuncelle = function(req,res){
+    cevapOlustur(res,200,{"durum":"basarili"});
+}
+const yorumGetir = function(req,res){
+   if(req.params && req.params.mekanid && req.params.yorumid) {
+    Mekan.findById(req.params.mekanid)
+    .select("ad yorumlar")
+    .exec(function(hata,mekan){
+        var cevap,yorum;
+        if(!mekan){
+            cevapOlustur(res,404,{
+                "hata":"Böyle bir mesaj yok"
+            });
+            return;
+        }else if (hata) {
+            cevapOlustur(res,400,hata);
+            return;
+        }
+        if(mekan.yorumlar && mekan.yorumlar.length > 0){
+            yorum = mekan.yorumlar.id(req.params.yorumid);
+            if(!yorum){
+                cevapOlustur(res,404,{
+                    "hata":"Böyle bir yorum yok!",
+                });
             }
-            if(mekan.yorumlar && mekan.yorumlar.length>0){
-                yorum = mekan.yorumlar.id(req.params.yorumid);
-                if(!yorum){
-                    cevapOlustur(res,404,{"hata":"Böyle bir yorum yok!"});
-                }else{
-                    cevap = {
-                        mekan: {
-                            ad: mekan.ad,
-                            id: req.params.mekanid
-                        },
-                        yorum: yorum
-                    };
-                    cevapOlustur(res,200,cevap);
-                }
-            }else{
-                cevapOlustur(res,404,{"hata": "Hiç yorum yok"});
+            else {
+                cevap = {
+                    mekan: {
+                        ad:mekan.ad,
+                        id:req.params.mekanid,
+                    },
+                    yorum: yorum,
+                };
+                cevapOlustur(res,200,cevap);
             }
-        });
-    }else {
-        cevapOlustur(res,404,{"hata": "Bulunamadı. mekanid ve yorumid mutlaka girilmeli"});
-    }
-};
-
+        } else {
+            cevapOlustur(res,404,{"hata":"Hiç yorum yok",
+            });
+        }
+    });
+   }else {
+    cevapOlustur(res,404,{
+        "hata":"Bulunamadı. mekanid ve yorumid mutlaka girilmeli",
+    });
+   }
+}
 module.exports={
     yorumEkle,
     yorumSil,
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,8 +1,7 @@
 var mongoose=require("mongoose");
 require("./mekansema");
 //var dbURI="mongodb://localhost/mekanbul";
-var dbURI="mongodb+srv://seyithan:73948426@mekanbul.w6je8ez.mongodb.net/mekanbul?retryWrites=true&w=majority"
-
+var dbURI="mongodb+srv://seyithan:73948426@mekanbul.w6je8ez.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 function kapat(msg,callback){
     mongoose.connection.close(function(){
