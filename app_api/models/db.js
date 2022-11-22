b465537a2b2811883601e81adf283f87204e467c
seyithansaltturk <seyithansaltturk@gmail.com>
2022-11-22 16:53:59 +0300

    odev 5 gönderildi

---
+++ app_api/models/db.js
@@ -0,0 +1,29 @@
+var mongoose=require("mongoose");
+require("./mekansema");
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI="mongodb+srv://seyithan:73948426@mekanbul.w6je8ez.mongodb.net/mekanbul?retryWrites=true&w=majority"
+
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback(); 
+    });
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+        process.exit(0);
+    })
+})
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresindeki veritabanına bağlandı");
+}
+);
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
+}
+);
+mongoose.connection.on("error",function(){
+    console.log("Bağlantı hatası");
+}
+);
